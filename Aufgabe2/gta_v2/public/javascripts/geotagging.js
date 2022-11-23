0468b02738eea61187ac27ab1d8710f66d5f2c9b
Sara Al Shihabi <sara@codecreators.de>
2022-11-23 12:35:20 +0100

    :bug: syntax bug in geotagging fixed

--- Aufgabe2/gta_v2/public/javascripts/geotagging.js
+++ Aufgabe2/gta_v2/public/javascripts/geotagging.js
@@ -103,17 +103,17 @@ class MapManager {
  * A function to retrieve the current location and update the page.
  * It is called once the page has been fully loaded.
  */
-    updateLocation(callback) {
+   function updateLocation(callback) {
     const geoLocationApi = navigator.geolocation;
     if (!geoLocationApi) {
         throw new Error("The GeoLocation API is unavailable.");
     }
-    
+
     helper.findLocation;
-    geoLocationApi.updateLocation((location) =>{
+    geoLocationApi.updateLocation((location) => {
+        let helper = new LocationHelper();
         helper.latitude = location.latitude;
         helper.longitude = location.longitude;
-        let helper = new LocationHelper();
         callback(helper);
     } , (error) => {
         alert(error.message)
