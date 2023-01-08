4e639c4e70478aed0014097ff0cb28b2eb3f1812
PlantSpaced <wije1012@h-ka.de>
2023-01-08 17:40:45 +0100

    .

--- Aufgabe4/gta_v4_template/public/javascripts/map-manager.js
+++ Aufgabe4/gta_v4_template/public/javascripts/map-manager.js
@@ -30,12 +30,13 @@ class MapManager {
             return "images/mapview.jpg";
         }
 
-        let tagList = `${latitude},${longitude}|marker-start`;
+        let tagList = `You,${latitude},${longitude}`;
+
         if(tags.length > 0) {
             
             console.log("Vor Reduce:", tags);
-            tagList +=tags.reduce((acc, tag) => `${acc}||${tag.latitude},${tag.longitude}|flag-${tag.name}`, "");
-            //tagList += tags.reduce((acc, tag) => `${acc}||${tag.name},${tag.latitude},${tag.longitude}`, "")
+            //tagList +=tags.reduce((acc, tag) => `${acc}||${tag.latitude},${tag.longitude}|flag-${tag.name}`, "");
+            tagList += Array.from(tags).reduce((acc, tag) => `${acc}|${tag.name},${tag.latitude},${tag.longitude}`, "");
 
         }
 
@@ -48,8 +49,6 @@ class MapManager {
 
         }
         */
-        
-       
         
         const mapQuestUrl = `https://www.mapquestapi.com/staticmap/v5/map?key=${this.#apiKey}&size=600,400&zoom=${zoom}&center=${latitude},${longitude}&locations=${tagList}`;
         console.log("Generated MapQuest URL:", mapQuestUrl);
